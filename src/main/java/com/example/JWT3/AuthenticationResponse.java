e1f01565a5cb83f1797a817aff5da2afbecaac24
khwaish <khwaish.tongiya@addverb.com>
2023-03-16 11:27:50 +0530

    BASIC SETUP DONE

---
+++ src/main/java/com/example/JWT3/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.example.JWT3;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
